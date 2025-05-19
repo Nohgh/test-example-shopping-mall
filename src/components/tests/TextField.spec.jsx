10b8e3ddcec585b50bdf69ab0fd4a17c464ed1af
노 기 훈 <66203019+Nohgh@users.noreply.github.com>
2025-05-19 16:54:15 +0900

    Test: TextField placeholder 단위테스트 작성

--- src/components/tests/TextField.spec.jsx
+++ src/components/tests/TextField.spec.jsx
@@ -3,6 +3,22 @@ import React from 'react';
 
 import TextField from '@/components/TextField';
 import render from '@/utils/test/render';
+
+beforeEach(() => {
+  console.log('root - beforeEach');
+});
+
+beforeAll(() => {
+  console.log('root - beforeAll');
+});
+
+afterEach(() => {
+  console.log('root - afterEach');
+});
+
+afterAll(() => {
+  console.log('root - afterAll');
+});
 /**
  * 1. Arrange - 테스트를 위한 환경 만들기
  * -> className을 지닌 컴포넌트 렌더링
@@ -33,6 +49,9 @@ describe('className', () => {
 });
 
 describe('placeholder', () => {
+  beforeEach(() => {
+    console.log('placeholder - beforeEach');
+  });
   it('기본 placeholder "텍스트를 입력해 주세요."가 노출된다.', async () => {
     // it: 기대결과를 정의한다.
     // 기대결과 === 실제 결과 -> 성공
@@ -55,5 +74,67 @@ describe('placeholder', () => {
     const textInput = screen.getByPlaceholderText('상품명을 입력해 주세요.');
 
     expect(textInput).toBeInTheDocument();
+  });
+
+  it('텍스트를 입력하면 onChange prop으로 등록한 함수가 호출된다.', async () => {
+    // vi.fn() - spy 함수를 만드는데 사용된다.
+    // spy 함수 - 테스트 코드에서 특정 함수가 호출되었는지,
+    // 함수의 인자로 어떤것이 넘어왔는지 어떤 값을 반환하는지 등 다양한 값을 저장
+    const spy = vi.fn();
+
+    const { user } = await render(<TextField onChange={spy} />);
+
+    const textInput = screen.getByPlaceholderText('텍스트를 입력해 주세요.');
+
+    await user.type(textInput, 'test'); //type api 사용하여 실제 텍스트 입력 시뮬레이션
+    // type 함수 -  내부적으로 킷다운 이벤트를 발생시킨다.
+
+    //입력한 값이 onChange이벤트 핸들러에 제대로 전달되는지 확인 -> vi.fn 사용
+
+    expect(spy).toHaveBeenCalledWith('test');
+    // - 스파이 함수가 내가 원하는 'test'란 문자열과 함께 올바르게 호출되었는지 단원할 수 있다.
+  });
+
+  it('엔터키를 입력하면 onEnter prop으로 등록한 함수가 호출된다.', async () => {
+    const spy = vi.fn();
+
+    const { user } = await render(<TextField onEnter={spy} />);
+
+    const textInput = screen.getByPlaceholderText('텍스트를 입력해 주세요.');
+
+    await user.type(textInput, 'test{Enter}'); //중괄호 내부에 Enter를 작성해 엔터키 입력 이벤트를 발생
+
+    expect(spy).toHaveBeenCalledWith('test');
+  });
+
+  it('포커스가 활성화되면 onFocus prop으로 등록한 함수가 호출된다.', async () => {
+    // 포커스 활성화 방법
+    // 1. 탭 키로 인풋 요소로 포커스 이동
+    // 2. 인풋 요소를 클릭
+    // 3. textInput.focus()로 직접 발생
+    const spy = vi.fn();
+
+    const { user } = await render(<TextField onFocus={spy} />);
+
+    const textInput = screen.getByPlaceholderText('텍스트를 입력해 주세요.');
+
+    await user.click(textInput);
+
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('포커스가 활성화되면 border 스타일이 추가된다.', async () => {
+    const spy = vi.fn();
+
+    const { user } = await render(<TextField onFocus={spy} />);
+
+    const textInput = screen.getByPlaceholderText('텍스트를 입력해 주세요.');
+
+    await user.click(textInput);
+
+    expect(textInput).toHaveStyle({
+      borderWidth: 2,
+      borderColor: 'rgb(25, 118, 210)',
+    });
   });
 });
--- src/utils/test/render.jsx
+++ src/utils/test/render.jsx
@@ -1,11 +1,15 @@
 import { render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
+/**
+ * useEvent -  click, mouse등 브라우저의 동작과 유사하게 시뮬레이션 할 수 있는 라이브러리
+ * setup - 이벤트를 시뮬레이션을 하기 위한 함수, 반환받은 인스턴스로 api를 사용할 수 있다.
+ */
 export default async component => {
   const user = userEvent.setup();
 
   return {
-    user,
+    user, //user의 api를 통해 사용자의 동작을 시뮬레이션
     ...render(component),
   };
 };
